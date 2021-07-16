72094b742618840f4bb90f3e74e2a95515f07e04
Josh Gumperz <joshg3023@gmail.com>
2021-07-15 22:08:46 -0700

    added inquirer, fs, and gitignore to repo, added code to prompt questions and generate a readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# readme-generator
+{
+/t"projectName": "Hello",
+/t"description": "My",
+/t"installation": "Name",
+/t"usage": "Is",
+/t"contribution guidelines": "Josh"
+}
--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Enter project name", "Describe your project", "How do I install the app?", "How do I use the app?", "Are there contribution guidelines",];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: questions[0],
+                name: "projectName",
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "description",
+            },
+            {
+                type: "input",
+                message: questions[2],
+                name: "installation",
+            },
+            {
+                type: "input",
+                message: questions[3],
+                name: "usage",
+            },
+            {
+                type: "input",
+                message: questions[4],
+                name: "contribution guidelines",
+            },
+        ])
+        .then ((data) => {
+            console.log(data)
+            const fileName = "README.md"
+            fs.writeFile(fileName, JSON.stringify(data, null, "/t"), (err) =>
+            err ? console.log(err) : console.log("yay!")
+            )
+        })
+}
 
 // Function call to initialize app
 init();
